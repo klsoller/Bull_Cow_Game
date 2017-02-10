dd5d14cee2cd2e523becd9937f7733247bcf5ff0
Kaleb Soller <kalebsoller@gmail.com>
2017-02-09 21:34:00 -0500

    second commit

--- main.cpp
+++ main.cpp
@@ -3,34 +3,44 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// Entry point for our application
 int main()
 {
-	// introduce the game
-	constexpr int WORD_LENGTH = 5;
-	cout << "Welcome to Bulls and Cows" << endl;
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of? \n";
+	PrintIntro();
 
 	// get a guess from the player
-	cout << "Enter your guess: ";	
-	string Guess = "";
-	cin >> Guess;
-	
-	// repeat the guess back to the player
-	cout << "Your guess was " << Guess << "\n";
-
-	
-
-	// get a guess from the player
-	cout << "Enter your guess: ";
-	cin >> Guess;
-
-	// repeat the guess back to the player
-	cout << "Your guess was " << Guess << "\n";
-
-
-
+	int limit = 5;
+	for (int count = 1; count <= limit; count++)
+	{
+		GetGuessAndPrintBack();
+	}
 
 
 	return 0;
 }
 
+
+void PrintIntro() {
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of? \n";
+	cout << endl;
+	return;
+}
+
+
+string GetGuessAndPrintBack() {
+	// get a guess from the player
+	cout << "Enter your guess:	";
+	string Guess = "";
+	getline(cin, Guess);
+
+	// repeat the guess back to the player
+	cout << "Your guess was . . . " << Guess << "\n";
+
+	return Guess;
+}
