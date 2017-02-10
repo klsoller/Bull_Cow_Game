5dbe73a03d1affc0e7c14981df870dd8fcdec1e1
Kaleb Soller <kalebsoller@gmail.com>
2017-02-09 19:43:46 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	// introduce the game
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of? \n";
+
+	// get a guess from the player
+	cout << "Enter your guess: ";	
+	string Guess = "";
+	cin >> Guess;
+	
+	// repeat the guess back to the player
+	cout << "Your guess was " << Guess << "\n";
+
+	
+
+	// get a guess from the player
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	// repeat the guess back to the player
+	cout << "Your guess was " << Guess << "\n";
+
+
+
+
+
+	return 0;
+}
+
